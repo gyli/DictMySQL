d3e87db895665ca0fd31c7dc58d24aef611cff1b
Guangyang Li <mail@guangyangli.com>
2017-03-09 21:52:26 -0500

    0.6.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as f:
 
 setup(name='dictmysql',
 
-      version='0.6.1',
+      version='0.6.2',
 
       description='A MySQL class for more convenient database manipulations with Python dictionary.',
 
